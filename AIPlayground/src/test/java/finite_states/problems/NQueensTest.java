e052a628c20c166a9846c42163dca9f5d6209528
D392 <danilofrancati@gmail.com>
2016-03-05 12:36:04 +0100

    Added test for NQueens

---
+++ AIPlayground/src/test/java/finite_states/problems/NQueensTest.java
@@ -0,0 +1,53 @@
+package finite_states.problems;
+
+import junit.framework.TestCase;
+
+public class NQueensTest extends TestCase {
+
+    public final int n = 4;
+    public final static String PROBLEM_TEST_NAME = "test_n_queens";
+    public final NQueens queens_problem = new NQueens(PROBLEM_TEST_NAME, n);
+
+    public void testGoalState() throws NoSuchFieldException {
+        NQueens.NQueensState state = (NQueens.NQueensState) queens_problem.buildRandomState();
+
+        state.positions[0] = 1;
+        state.positions[1] = 3;
+        state.positions[2] = 0;
+        state.positions[3] = 2;
+        assertEquals(queens_problem.isGoal(state), true);
+
+        state.positions[0] = 2;
+        assertEquals(queens_problem.isGoal(state), false);
+
+        state.positions[0] = 3;
+        state.positions[1] = 0;
+        state.positions[2] = 1;
+        state.positions[3] = 2;
+        assertEquals(queens_problem.isGoal(state), false);
+
+        state.positions[0] = 1;
+        state.positions[1] = 3;
+        state.positions[2] = 1;
+        state.positions[3] = 1;
+        assertEquals(queens_problem.isGoal(state), false);
+
+        state.positions[0] = 0;
+        state.positions[1] = 1;
+        state.positions[2] = 2;
+        state.positions[3] = 3;
+        assertEquals(queens_problem.isGoal(state), false);
+
+        state.positions[0] = 3;
+        state.positions[1] = 2;
+        state.positions[2] = 1;
+        state.positions[3] = 0;
+        assertEquals(queens_problem.isGoal(state), false);
+
+        state.positions[0] = 3;
+        state.positions[1] = 1;
+        state.positions[2] = 2;
+        state.positions[3] = 0;
+        assertEquals(queens_problem.isGoal(state), false);
+    }
+}
