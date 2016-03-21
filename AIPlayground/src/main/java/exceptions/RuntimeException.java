e461ae7ccbfab46a52f5f332fd9b418759bd8900
Aldur <adrianodl@hotmail.it>
2016-03-21 20:17:36 +0100

    Fix octal integer misintepretation

--- AIPlayground/src/main/java/exceptions/RuntimeException.java
+++ AIPlayground/src/main/java/exceptions/RuntimeException.java
@@ -4,7 +4,7 @@ package exceptions;
  * A generic exception happened at runtime.
  */
 public class RuntimeException extends Exception {
-    private static final long serialVersionUID = 01L;
+    private static final long serialVersionUID = 1L;
 
     public RuntimeException(String message) {
         super(message);
